099bfa26129acafe0f641e5cd9e79155b275c7e2
em1lyly77 <56006157+em1lyly77@users.noreply.github.com>
2023-02-19 20:31:40 -0500

    updated lr scheduler

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,25 +6,26 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
-
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 20
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "T_max": 20,
+        "gamma": 0.8,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
-        [
-            ToTensor(),
-        ]
+        [ToTensor(), Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,16 @@
 from typing import List
+import math
 
 from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Define a custom learning rate scheduler that first decays and then enters cosine annealing
+    """
+
+    def __init__(self, optimizer, gamma, T_max, eta_min=0, last_epoch=-1):
+        # gamma
         """
         Create a new scheduler.
 
@@ -13,12 +19,42 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
+        # self.num_epochs = num_epochs
+        self.T_max = T_max
+        self.eta_min = eta_min
+        self.gamma = gamma
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
+        """
+        updates learning rate accordingly: exponentially at first, then cosine annealing
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
-        # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # return [i for i in self.base_lrs]
+
+        # ... Your Code Here ...
+
+        # self.base_lrs all are 0.001 (initial lr)
+        # exponential decay
+        # return [base_lr * math.exp(self.gamma) for base_lr in self.base_lrs]
+        if self.last_epoch < 500:
+            return [
+                base_lr * math.exp(-self.gamma * self.last_epoch)
+                for base_lr in self.base_lrs
+            ]
+
+        else:
+            return [
+                1e-4
+                + (base_lr - self.eta_min)
+                * 1
+                / 2
+                * (1 + math.cos(math.pi * self.last_epoch / self.T_max))
+                for base_lr in self.base_lrs
+            ]
+
+        # cosine annealing
+        # return [(1 + math.cos())]
