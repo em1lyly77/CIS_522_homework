e6072d9385258d18ea7b07a951fa5bb97aff796d
em1lyly77 <56006157+em1lyly77@users.noreply.github.com>
2023-04-27 01:29:27 -0400

    remove print

--- assignments/05-RL/main.py
+++ assignments/05-RL/main.py
@@ -16,6 +16,7 @@ agent = Agent(
     observation_space=env.observation_space,
 )
 
+
 total_reward = 0
 last_n_rewards = []
 for _ in range(100000):
@@ -30,11 +31,11 @@ for _ in range(100000):
         n = min(30, len(last_n_rewards))
         avg = sum(last_n_rewards[-n:]) / n
         improvement_emoji = "🔥" if (total_reward > avg) else "😢"
-        print(
-            f"{improvement_emoji} Finished with reward {int(total_reward)}.\tAverage of last {n}: {int(avg)}"
-        )
-        if avg > 0:
-            print("🎉 Nice work! You're ready to submit the leaderboard! 🎉")
+        # print(
+        #     f"{improvement_emoji} Finished with reward {int(total_reward)}.\tAverage of last {n}: {int(avg)}"
+        # )
+        # if avg > 0:
+        #     print("🎉 Nice work! You're ready to submit the leaderboard! 🎉")
         total_reward = 0
 
 env.close()
